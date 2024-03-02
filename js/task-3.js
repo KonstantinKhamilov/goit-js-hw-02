76677396732213f2205ceb1843255f9aaf459b90
KonstantinKhamilov <134136455+KonstantinKhamilov@users.noreply.github.com>
2024-03-02 15:33:14 +0200

    Выполнены все задания

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,19 @@
 `use strict`;
 
-function checkForSpam(message) {}
+function checkForSpam(message) {
+  let result = message.toLocaleLowerCase();
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+  if (result.includes(`spam`) || result.includes(`sale`)) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+checkForSpam("Latest technology news"); // false
+checkForSpam("JavaScript weekly newsletter"); // false
+checkForSpam("Get best sale offers now!"); // true
+checkForSpam("Amazing SalE, only tonight!"); // true
+checkForSpam("Trust me, this is not a spam message"); // true
+checkForSpam("Get rid of sPaM emails. Our book in on sale!"); // true
+checkForSpam("[SPAM] How to earn fast money?"); // true
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,38 @@
 `use strict`;
+
+function getShippingCost(country) {
+  let price;
+
+  switch (country) {
+    case `Australia`:
+      price = 170;
+      break;
+    case `China`:
+      price = 100;
+      break;
+    case `Chile`:
+      price = 250;
+      break;
+    case `Jamaica`:
+      price = 120;
+      break;
+  }
+
+  if (
+    country == `Australia` ||
+    country == `China` ||
+    country == `Chile` ||
+    country == `Jamaica`
+  ) {
+    console.log(`Shipping to ${country} will cost ${price} credits`);
+  } else {
+    console.log(`Sorry, there is no delivery to your country`);
+  }
+}
+
+getShippingCost("Australia"); // "Shipping to Australia will cost 170 credits"
+getShippingCost("Germany"); // "Sorry, there is no delivery to your country"
+getShippingCost("China"); // "Shipping to China will cost 100 credits"
+getShippingCost("Chile"); // "Shipping to Chile will cost 250 credits"
+getShippingCost("Jamaica"); // "Shipping to Jamaica will cost 120 credits"
+getShippingCost("Sweden"); // "Sorry, there is no delivery to your country"
